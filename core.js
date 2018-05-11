26b2d7bc9792211e40b4b1b6cb3cc482df273f72
CircuitLord <circuitlordmc@gmail.com>
2018-05-11 14:17:43 -0500

    Config loading system from database

--- core.js
+++ core.js
@@ -7,12 +7,43 @@ const fs = require("fs");
 
 const MUDFROG = new Discord.Client();
 
+
+//Initialize all the sub parts of the MUDFROG core object.
 MUDFROG.utils = require("./utils/circuit-utils.js");
-MUDFROG.config = require("./config.json")
-MUDFROG.db = require("./utils/database.js")
+MUDFROG.config = require("./config.json");
+MUDFROG.configTemp = require("./configTemplate.json")
+MUDFROG.db = require("./utils/database.js");
+MUDFROG.activeConfigs = {};
+
+
+MUDFROG.on("ready", function (message) {
 
 
 
-client.on("ready", function (message) {
+
+
+    //Load server configs.
+    function testGuildConfig(guild, index, arr) {
+        if (MUDFROG.db.configGet(guild.id) === null) {
+            MUDFROG.db.configSet(guild.id, MUDFROG.configTemp);  
+        }
+        return true;
+    }
+
+    MUDFROG.guilds.every(testGuildConfig);
+
+    MUDFROG.activeConfigs = MUDFROG.db.loadConfigs();
+
+
+
+
+
     console.log(`Mudfrog ${MUDFROG.config.botVersion} is ready to go.`);
 });
+
+
+
+
+
+
+MUDFROG.login(MUDFROG.config.token);
--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,9 @@ const e = require('enmap');
 const EnmapLevel = require('enmap-level');
 
 
-var enmap = new e({ provider: new EnmapLevel({ name: 'MudfrogDatabase' }) });
+var users = new e({ provider: new EnmapLevel({ name: 'usersDB' }) });
+
+var serverConfigs = new e({ provider: new EnmapLevel({ name: 'serverConfigs' }) });
 
 
 
@@ -13,4 +15,23 @@ exports.set = (key, value) => {
 
 exports.get = (key) => {
     return enmap.get(key);
+}
+
+exports.configSet = (key, value) => {
+    serverConfigs.set(key, value);
+}
+
+exports.configGet = (key) => {
+    return serverConfigs.get(key);
+}
+
+exports.loadConfigs = () => {
+    var configData = {};
+    var i = 0;
+    serverConfigs.map ((val, index, arr) => {
+        i++;
+        configData[index] = val;
+    });
+    console.log(`Getting configs from ${i} guilds.`)
+    return configData;
 }
